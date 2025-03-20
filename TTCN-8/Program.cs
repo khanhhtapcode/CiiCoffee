25ef64370b3650a6b2dd940f7ae02a745c397496
Mai Thai Huy <132258939+maithaihuy16102004@users.noreply.github.com>
2025-03-21 03:04:11 +0700

    Add files via upload

---
+++ TTCN-8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        Console.WriteLine("Chuỗi kết nối: " + connString);
+
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                Console.WriteLine("✅ Kết nối thành công!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("❌ Lỗi kết nối: " + ex.Message);
+        }
+    }
+}
---
+++ TTCN-8/README.md
@@ -0,0 +1,2 @@
+# TTCN-8
+Hệ thống Savour Cakes là ứng dụng quản lý tiệm bánh, phát triển bằng C# WinForms và tích hợp SQL Server. Hệ thống hỗ trợ quản lý sản phẩm, đơn hàng, khách hàng và nhân viên, giúp tối ưu hóa quy trình bán hàng, kiểm soát kho và nâng cao trải nghiệm khách hàng.
